47d2a117f9917edaceb2b6a206683c1b1208324c
Dylan Murray <dylan@britevue.com>
2025-10-21 09:10:25 -0400

    Add ban manager to registry

--- services/p2p/Server_test.go
+++ services/p2p/Server_test.go
@@ -117,7 +117,7 @@ func createTestServer(t *testing.T) *Server {
 		logger:       logger,
 		settings:     settings,
 		peerRegistry: NewPeerRegistry(),
-		banManager:   NewPeerBanManager(context.Background(), nil, settings),
+		banManager:   NewPeerBanManager(context.Background(), nil, settings, registry),
 	}
 
 	return s
@@ -3591,7 +3591,7 @@ func createEnhancedTestServer(t *testing.T) (*Server, *MockServerP2PClient, *Moc
 		logger:       logger,
 		settings:     settings,
 		peerRegistry: NewPeerRegistry(),
-		banManager:   NewPeerBanManager(context.Background(), nil, settings),
+		banManager:   NewPeerBanManager(context.Background(), nil, settings, registry),
 		P2PClient:    mockP2PNode,
 		banList:      mockBanList,
 		gCtx:         context.Background(),
